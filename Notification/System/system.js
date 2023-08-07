ec950605b190ab4eaa032df5aad2fbf57f6b5fc7
natali-alkayed <nataly2000alkayed@gmail.com>
2023-08-07 08:57:34 +0000

    finishing notification system

--- Notification/System/system.js
+++ Notification/System/system.js
@@ -1,62 +1,15 @@
-// const express = require('express');
-// const app = express();
-// const httpServer = require('http').createServer(app);
-// const io = require('socket.io')(httpServer);
-// const port = process.env.PORT || 3000;
-
-// httpServer.listen(port, () => {
-//   console.log(`Main server is running on port ${port}`);
-// });
-
-// io.on('connection', (socket) => {
-//   console.log('A user connected.');
-
-//   socket.on('join-renter', () => {
-//     console.log('Renter joined the system.');
-//   });
-
-//   socket.on('join-shipper', () => {
-//     console.log('Shipper joined the system.');
-
-//     // // When a shipper joins, send them the list of available chargers.
-//     // const chargers = [
-//     //   { id: 'charger1', name: 'Charger 1' },
-//     //   { id: 'charger2', name: 'Charger 2' },
-//     //   // Add more chargers as needed
-//     // ];
-//     // socket.emit('chargers', chargers);
-//   });
-
-//   socket.emit('send-request-for-charger', (chargerId) => {
-//     console.log(`Received a request from the renter for charger ${chargerId}`);
-//     socket.broadcast.emit('received-request-for-charger', chargerId);
-//   //  socket.on(`system received first notification`);
-//   });
-
-//   // socket.on('approve', (chargerId) => {
-//   //   console.log(`Shipper approved the request for charger with ID '${chargerId}'.`);
-//   //   socket.emit('charger-approval', chargerId, true);
-//   // });
-
-//   // socket.on('cancel', (chargerId) => {
-//   //   console.log(`Shipper canceled the request for charger with ID '${chargerId}'.`);
-//   //   socket.emit('charger-approval', chargerId, false);
-//   // });
-
-//   // socket.on('took-off-log', (chargerId) => {
-//   //   console.log(`Charger with ID '${chargerId}' took off.`);
-//   //   socket.broadcast.emit('took-off-log', chargerId);
-//   // });
-
-//   socket.on('disconnect', () => {
-//     console.log('A user disconnected.');
-//   });
-// });
+'use strict';
 const express = require('express');
 const app = express();
 const httpServer = require('http').createServer(app);
 const io = require('socket.io')(httpServer);
 const port = process.env.PORT || 3001;
+// const queue = {
+//   requests: {
+
+//   }
+// };
+
 
 httpServer.listen(port, () => {
   console.log(`Main server is running on port ${port}`);
@@ -65,24 +18,36 @@ httpServer.listen(port, () => {
 io.on('connection', (socket) => {
   console.log('A user connected.');
 
+
+  
   socket.on('join-renter', () => {
     console.log('Renter joined the system.');
   });
 
   socket.on('join-shipper', () => {
-    console.log('Shipper joined the system.');
+    console.log('Provider joined the system.');
+  //  console.log("*********************",chargerId);
   });
 
   socket.on('send-request-for-charger', (chargerId) => {
-    console.log(`renter sent a request for the shipper to rent the charger ${chargerId}`);
+    console.log(`Renter sent a request for the shipper to rent the charger ${chargerId}`);
     socket.broadcast.emit('received-request-for-charger', chargerId);
+   
   });
   io.emit('send-request-for-charger-from-renter-side')
   io.emit('system received first notification')
 
   socket.on('shipper-accepted-request', (chargerId) => {
-    console.log(`shipper accepted the renter request for charger ${chargerId}`);
+    console.log(`Provider accepted the renter request for charger ${chargerId}`);
   });
+
+
+  // socket.on('get-all', () => {
+  //   const flights = Object.values(queue.requests);
+  //   //socket.emit('flight', requests);
+  //   queue.requests = {};
+  //   console.log("*****************************",queue.requests);
+  // });
 
   socket.on('disconnect', () => {
     console.log('A user disconnected.');
