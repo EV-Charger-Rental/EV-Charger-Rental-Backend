ca20f8754296b3e9a9e0853c3aaee5ca7903ba41
ZainabMalkawi94 <zainab.m.malkawi@gmail.com>
2023-10-02 08:10:31 +0000

    Editing Relations between Charger module and user module to connect the two modules through username

--- src/models/charger/charger.js
+++ src/models/charger/charger.js
@@ -3,7 +3,7 @@
 const chargerModel = (sequelize, DataTypes) => sequelize.define('Charger', {
   ChargerType: { type: DataTypes.STRING, required: true },
   status: { type: DataTypes.STRING, required: true },
-  username: { type: DataTypes.STRING, primaryKey: true }, // Change the primary key to 'username'
+  username: { type: DataTypes.STRING}, // Change the primary key to 'username'
   price: { type: DataTypes.FLOAT, required: true },
   Chargerlocation: { type: DataTypes.STRING, required: true }
 });
