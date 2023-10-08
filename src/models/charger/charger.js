dda1cbf6efcde315a0a3620e55e85583c69ae3b7
mohamadshawabkeh <mohamadshawabkeh45@outlook.com>
2023-10-08 11:15:24 +0300

    chargerUpdate

--- src/models/charger/charger.js
+++ src/models/charger/charger.js
@@ -4,10 +4,13 @@ const dayjs = require('dayjs');
 
 const chargerModel = (sequelize, DataTypes) => sequelize.define('Charger', {
   ChargerType: { type: DataTypes.STRING, required: true },
-  status: { type: DataTypes.STRING, required: true, },
+  status: { type: DataTypes.STRING, required: true },
   owner_id: { type: DataTypes.INTEGER, required: true },
   price: { type: DataTypes.FLOAT, required: true },
-  Chargerlocation: { type: DataTypes.STRING, required: true }
+  latitude: { type: DataTypes.FLOAT, allowNull: false },
+  longitude: { type: DataTypes.FLOAT, allowNull: false },
+  chargerAddress: { type: DataTypes.STRING, allowNull: false }, // Add chargerAddress column
 });
 
 module.exports = chargerModel;
+
