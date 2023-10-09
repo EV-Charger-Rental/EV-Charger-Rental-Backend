1f8222efa022524b84102cb8a07cc4e7757882f5
ZainabMalkawi94 <zainab.m.malkawi@gmail.com>
2023-10-09 14:20:05 +0000

    reservation model-delete all things below the resevation model

--- src/models/reservations/reservations.js
+++ src/models/reservations/reservations.js
@@ -1,23 +1,19 @@
-'use strict';
+"use strict";
+
 const dayjs = require('dayjs');
 
-const reservationModel = (sequelize, DataTypes) => {
-  const Reservation = sequelize.define('Reservation', {
-    charger_id: { type: DataTypes.INTEGER, allowNull: false },
-    renter_id: { type: DataTypes.INTEGER, allowNull: false },
-    Provider_id: { type: DataTypes.INTEGER, allowNull: false },
-    creation_date: { type: DataTypes.DATE, allowNull: true },
-    start_time: { type: DataTypes.STRING, allowNull: false }, // Change data type to STRING
-    end_time: { type: DataTypes.STRING, allowNull: false },   // Change data type to STRING
-    total_price: { type: DataTypes.FLOAT, allowNull: true },
-    reservation_status: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      defaultValue: 'open',
-    },
-  });
-
-  return Reservation;
-};
+const reservationModel = (sequelize, DataTypes) => sequelize.define('Reservation', {
+  charger_id: { type: DataTypes.INTEGER, allowNull: false },
+  renter_id: { type: DataTypes.INTEGER, allowNull: false },
+  Provider_id: { type: DataTypes.INTEGER, allowNull: false },
+  start_time: { type: DataTypes.STRING, allowNull: false }, // Change data type to STRING
+  end_time: { type: DataTypes.STRING, allowNull: false },   // Change data type to STRING
+  total_price: { type: DataTypes.FLOAT, allowNull: true },
+  reservation_status: {
+    type: DataTypes.STRING,
+    allowNull: true,
+    defaultValue: 'open',
+  },
+});
 
 module.exports = reservationModel;
